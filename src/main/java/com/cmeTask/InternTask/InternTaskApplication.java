fb2d827dd1ae3490f91098ddc954dacf24570f43
Marc Maalouly <marc.maalouly771122@gmail.com>
2020-12-24 11:28:01 +0200

    First commit with Database Person created

---
+++ src/main/java/com/cmeTask/InternTask/InternTaskApplication.java
@@ -0,0 +1,13 @@
+package com.cmeTask.InternTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cmeTask/InternTask/InternTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmeTask.InternTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
